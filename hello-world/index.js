64bb9bf39a495e676fd0556565528ee2b37c9454
Emily Shouppe <emily.shouppe@storicard.com>
2021-10-11 10:06:41 -0400

    Rename js ,file

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,9 +1,9 @@
-require('/mnt/efs/node/node_modules/app-module-path').addPath('/mnt/efs/node/node_modules');
+require('/mnt/efs/app-module-path').addPath('/mnt/efs/node/node_modules');
 var _ = require ('lodash');
 const { DateTime } = require('luxon');
 
 
-exports.lambdaHandler =  async function(event, context) {
+exports.handler = async function(event, context) {
     console.log("Received event: \n" + JSON.stringify(event, null, 2))
     console.log(_.chunk(['a', 'b', 'c', 'd'], 2));
     console.log(DateTime.now().setZone('America/New_York').minus({weeks:1}).endOf('day').toISO());
